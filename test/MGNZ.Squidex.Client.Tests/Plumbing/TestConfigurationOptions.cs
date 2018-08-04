cef97d1cdc1873114e7ec708a0aea52cfb9f9eef
Steven Higgan <stevenh@mad.geek.nz>
2018-08-04 21:44:03 +1200

    configuration option handling handling

--- test/MGNZ.Squidex.Client.Tests/Plumbing/TestConfigurationOptions.cs
+++ test/MGNZ.Squidex.Client.Tests/Plumbing/TestConfigurationOptions.cs
@@ -1,14 +1,14 @@
-using System;
-using System.Collections.Generic;
-using System.Text;
-
 namespace MGNZ.Squidex.Client.Tests.Plumbing
 {
+  using System;
+  using System.Collections.Generic;
+
   using MGNZ.Squidex.Client.Tests.Stories;
 
   public class TestConfigurationOptions
-    {
-      public Uri BaseAddressUri { get; set; }
-      public Dictionary<string, KnownUserOAuthCreds> Clients { get; set; }
-    }
+  {
+    public Uri BaseAddressUri { get; set; }
+    public string AdministratorToken { get; set; }
+    public Dictionary<string, KnownUserOAuthCreds> Clients { get; set; }
+  }
 }
