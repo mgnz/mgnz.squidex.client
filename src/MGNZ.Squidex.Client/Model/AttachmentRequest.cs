a8dabb102b5842d07e790ad3480fc2989089d551
Steven Higgan <stevenh@mad.geek.nz>
2018-11-22 23:22:44 +1300

    updating defaults for attachmentrequest dto

--- src/MGNZ.Squidex.Client/Model/AttachmentRequest.cs
+++ src/MGNZ.Squidex.Client/Model/AttachmentRequest.cs
@@ -4,9 +4,10 @@ namespace MGNZ.Squidex.Client.Model
 
   public class AttachmentRequest
   {
-    [JsonProperty(PropertyName = "version")] public int Version { get; set; }
-    [JsonProperty(PropertyName = "width")] public int Width { get; set; }
-    [JsonProperty(PropertyName = "height")] public int Height { get; set; }
+    // todo : see AssetContentController.GetAssetContent
+    [JsonProperty(PropertyName = "version")] public int Version { get; set; } = -1;
+    [JsonProperty(PropertyName = "width")] public int? Width { get; set; }
+    [JsonProperty(PropertyName = "height")] public int? Height { get; set; }
     [JsonProperty(PropertyName = "mode")] public string Mode { get; set; }
 
   }
