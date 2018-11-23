f1181d4a0179cee407296aaddb0f5b4bb125db22
Steven Higgan <stevenh@mad.geek.nz>
2018-11-24 01:56:09 +1300

    Integration tests for GetAsset, DeleteAsset, GetAssets
    #4

--- test/MGNZ.Squidex.Client.Tests/ISquidexAttachmentClientIntegrationTest.cs
+++ test/MGNZ.Squidex.Client.Tests/ISquidexAttachmentClientIntegrationTest.cs
@@ -1,9 +1,6 @@
-using System;
-using System.Collections.Generic;
-using System.Text;
-
 namespace MGNZ.Squidex.Client.Tests
 {
+  using System;
   using System.IO;
   using System.Linq;
   using System.Net.Http;
@@ -13,6 +10,7 @@ namespace MGNZ.Squidex.Client.Tests
   using Bogus;
 
   using MGNZ.Squidex.Client.Handlers;
+  using MGNZ.Squidex.Client.Model;
   using MGNZ.Squidex.Client.Tests.Plumbing;
   using MGNZ.Squidex.Client.Tests.Shared.Assets;
   using MGNZ.Squidex.Client.Tests.Shared.Code;
@@ -90,14 +88,35 @@ namespace MGNZ.Squidex.Client.Tests
       await AttachmentClient.AssertNoAttachmentsExist("aut", delay: TimeSpan.FromSeconds(2));
     }
 
-    [Fact(Skip = "in progress")]
+    [Fact]
     public async Task GetAsset_Execute_EndToEnd()
     {
+      var attachmentName = $"{base.GetRandomSchemaName}.jpg";
+
+      await AttachmentClient.AssertNoAttachmentsExist("aut");
+      var createResponse = await AttachmentClient.Post("aut", attachmentName, "image/jpeg", AssetLoader.Asset2);
+      await AttachmentClient.AssertAttachmentMustExist("aut", attachmentName, delay: TimeSpan.FromSeconds(2));
+
+      var id = createResponse.Id;
+      var getAssetResponse = await AttachmentClient.GetAsset("aut", id);
+      // todo : assert the updatedresponse matches 
+
+      var deleteResponse = await AttachmentClient.DeleteAsset("aut", id);
+      await AttachmentClient.AssertNoAttachmentsExist("aut", delay: TimeSpan.FromSeconds(2));
     }
 
-    [Fact(Skip = "in progress")]
+    [Fact]
     public async Task DeleteAsset_Execute_EndToEnd()
     {
+      var attachmentName = $"{base.GetRandomSchemaName}.jpg";
+
+      await AttachmentClient.AssertNoAttachmentsExist("aut");
+      var createResponse = await AttachmentClient.Post("aut", attachmentName, "image/jpeg", AssetLoader.Asset2);
+      await AttachmentClient.AssertAttachmentMustExist("aut", attachmentName, delay: TimeSpan.FromSeconds(2));
+
+      var id = createResponse.Id;
+      var deleteResponse = await AttachmentClient.DeleteAsset("aut", id);
+      await AttachmentClient.AssertNoAttachmentsExist("aut", delay: TimeSpan.FromSeconds(2));
     }
 
     [Fact(Skip = "in progress")]
@@ -105,9 +124,33 @@ namespace MGNZ.Squidex.Client.Tests
     {
     }
 
-    [Fact(Skip = "in progress")]
+    [Fact]
     public async Task GetAssets_Execute_EndToEnd()
     {
+      var attachmen1tName = $"{base.GetRandomSchemaName}.jpg";
+      var attachment2Name = $"{base.GetRandomSchemaName}.jpg";
+
+      await AttachmentClient.AssertNoAttachmentsExist("aut");
+
+      var createResponse1 = await AttachmentClient.Post("aut", attachmen1tName, "image/jpeg", AssetLoader.Asset2);
+      await AttachmentClient.AssertAttachmentMustExist("aut", attachmen1tName, delay: TimeSpan.FromSeconds(2));
+      var createResponse2 = await AttachmentClient.Post("aut", attachment2Name, "image/jpeg", AssetLoader.Asset3);
+      await AttachmentClient.AssertAttachmentMustExist("aut", attachment2Name, delay: TimeSpan.FromSeconds(2));
+
+      var getAllResponse = await AttachmentClient.GetAssets("aut", new ListRequest()
+      {
+        Skip = 0,
+        Top = 2,
+      });
+
+      // todo : assert we got both
+
+      var id1 = createResponse1.Id;
+      var id2 = createResponse2.Id;
+      var deleteResponse1 = await AttachmentClient.DeleteAsset("aut", id1);
+      var deleteResponse2 = await AttachmentClient.DeleteAsset("aut", id2);
+
+      await AttachmentClient.AssertNoAttachmentsExist("aut", delay: TimeSpan.FromSeconds(2));
     }
   }
 
